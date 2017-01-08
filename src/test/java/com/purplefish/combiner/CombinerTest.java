c06a31e8f4d9e655312d1ee777e60bd23a11c9b3
xuyue <xuyue@abcdeMacBook-Pro.local>
2017-01-08 13:18:22 +0800

    simple test

---
+++ src/test/java/com/purplefish/combiner/CombinerTest.java
@@ -0,0 +1,61 @@
+package com.purplefish.combiner;
+
+import com.purplefish.combiner.api.CombineRunner;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by xuyue on 2017/1/8.
+ */
+public class CombinerTest {
+
+    private ExecutorService pool = Executors.newFixedThreadPool(10);
+    private Combiner<String, String, Integer> combiner;
+    List<String> combineIds = new ArrayList<String>();
+
+    @Before
+    public void init() throws Exception {
+        combiner = Combiner.combinerBuilder().runner(new CombineRunner<String, String, Integer>(){
+            @Override
+            public boolean run(String id, Collection<SubmitData<String, Integer>> submitDatas) {
+                System.out.println("id is " + id + "; data's size is " + submitDatas.size());
+                return true;
+            }
+        }).maxRate(1).build();
+        combineIds.add("aaa");
+        combineIds.add("bbb");
+        combineIds.add("ccc");
+        combineIds.add("ddd");
+    }
+
+    @Test
+    public void test() throws InterruptedException {
+        for (int i = 0; i < 10000; ++i){
+            final String combineId = combineIds.get(i%combineIds.size());
+            final String id = Integer.toString(i);
+            final Integer value = i;
+            pool.submit(new Runnable() {
+                @Override
+                public void run() {
+                    combiner.submit(combineId, id, value);
+                }
+            });
+//            Thread.currentThread().sleep(1);
+        }
+        try {
+            Thread.currentThread().sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
